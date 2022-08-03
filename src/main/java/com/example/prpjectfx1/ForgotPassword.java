f42b4fb0eb0732fccd3cdc3c2ba980d50059be3d
Classic <Kordemilmelika@gmail.com>
2022-08-04 02:19:52 +0430

    good night

--- src/main/java/com/example/prpjectfx1/ForgotPassword.java
+++ src/main/java/com/example/prpjectfx1/ForgotPassword.java
@@ -1,31 +1,6 @@
 package com.example.prpjectfx1;
 
-import javafx.fxml.FXML;
-import javafx.scene.layout.BorderPane;
 
 public class ForgotPassword {
 
-    @FXML
-    BorderPane parent;
-    private boolean isLightMode = true;
-
-//    @FXML
-//    public void changeMode(){
-//        isLightMode = !isLightMode;
-//        if(isLightMode){
-//           setLightMode();
-//        }else {
-//            setDarkMode();
-//        }
-//    }
-//
-//    private void setLightMode(){
-//        parent.getStylesheets().remove(getClass().getResource("/com/styles/darkMode.css").toExternalForm());
-//        parent.getStylesheets().add(getClass().getResource("/com/styles/lightMode.css").toExternalForm());
-//    }
-//
-//    private void setDarkMode(){
-//        parent.getStylesheets().remove(getClass().getResource("/com/styles/lightMode.css").toExternalForm());
-//        parent.getStylesheets().add(getClass().getResource("/com/styles/darkMode.css").toExternalForm());
-//    }
 }
