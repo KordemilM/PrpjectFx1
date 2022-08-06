c3d739080a50cb329e544fcfdedd2ab16d75cebd
Maziar Goharkesh <maziar.gohar@gmail.com>
2022-08-06 09:46:53 +0430

    change password

--- src/main/java/com/example/prpjectfx1/repository/ConnectToTheDatabase.java
+++ src/main/java/com/example/prpjectfx1/repository/ConnectToTheDatabase.java
@@ -9,7 +9,7 @@ public class ConnectToTheDatabase {
     public static Connection ConnectDatabase() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/project","root","M78fF52Kwa1");
+                .getConnection("jdbc:mysql://localhost:3306/project","root","maziar.gohar123");
         return connection;
     }
 }
