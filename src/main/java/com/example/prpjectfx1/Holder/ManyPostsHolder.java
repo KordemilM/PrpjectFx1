88010d7bbbbce2fd0709585f262c435245c13976
Maziar Goharkesh <maziar.gohar@gmail.com>
2022-08-07 14:35:13 +0430

    add lombok annotation instead of raw getter & setter

--- src/main/java/com/example/prpjectfx1/Holder/ManyPostsHolder.java
+++ src/main/java/com/example/prpjectfx1/Holder/ManyPostsHolder.java
@@ -1,29 +1,23 @@
 package com.example.prpjectfx1.Holder;
 
 import com.example.prpjectfx1.entity.PostCom;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ManyPostsHolder {
     public static ArrayList<ManyPostsHolder> instances = new ArrayList<>();
-    private ArrayList<PostCom> posts;
-
-    public ManyPostsHolder() {
-        posts = new ArrayList<>();
-    }
+    private ArrayList<PostCom> posts = new ArrayList<>();
 
     public static ManyPostsHolder getInstance(int index) {
         if (instances.size() <= index) {
             instances.add(new ManyPostsHolder());
         }
         return instances.get(index);
-    }
-
-    public ArrayList<PostCom> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(ArrayList<PostCom> posts) {
-        this.posts = posts;
     }
 }
--- src/main/java/com/example/prpjectfx1/Holder/PostHolder.java
+++ src/main/java/com/example/prpjectfx1/Holder/PostHolder.java
@@ -1,18 +1,13 @@
 package com.example.prpjectfx1.Holder;
 
 import com.example.prpjectfx1.entity.PostCom;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PostHolder {
     private PostCom postCom;
     private static final PostHolder INSTANCE = new PostHolder();
-    public static PostHolder getInstance() {
-        return INSTANCE;
-    }
-    public PostCom getPostCom() {
-        return postCom;
-    }
-    public void setPostCom(PostCom postCom) {
-        this.postCom = postCom;
-    }
 
 }
